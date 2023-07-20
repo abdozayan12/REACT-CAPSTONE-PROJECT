b9e67d35f11fc8ac8a41f1ef1050b44c02dbd2ff
Abd elhakim Zayan <abdozayan12@gmailcom>
2023-07-20 22:22:15 +0300

    Add Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,17 @@
-const Header = () => <div>Header</div>;
+import GoBack from '../assets/images/GoBack.png';
+import Mic from '../assets/images/Mic.png';
+import Settings from '../assets/images/Settings.png';
 
+const Header = () => (
+  <header>
+    <nav>
+      <button type="button" className="navigation-icons"><img src={GoBack} alt="Go back" /></button>
+      <h1><a href="https://www.google.com/">Header</a></h1>
+      <div>
+        <button type="button" className="navigation-icons"><img src={Mic} alt="Mic" /></button>
+        <button type="button" className="navigation-icons"><img src={Settings} alt="Settings" /></button>
+      </div>
+    </nav>
+  </header>
+);
 export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,3 @@
-const Home = () => <div>Home</div>;
+const Home = () => <div>Home Page</div>;
 
 export default Home;
