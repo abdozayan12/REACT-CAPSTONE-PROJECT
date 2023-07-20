f4f0144f42f5d9e930b638f3f456f91091624f8d
Abd elhakim Zayan <abdozayan12@gmailcom>
2023-07-20 14:19:30 +0300

    Add basic structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Details from './pages/Details';
 
 function App() {
   return (
-    <div>Edit</div>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </div>
   );
 }
 
